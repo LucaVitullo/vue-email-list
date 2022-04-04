38713bad0f6addc285c9babecd0741346f226829
Lucavit9 <95365833+Lucavit9@users.noreply.github.com>
2022-04-04 15:04:12 +0200

    generate le 10 mail

---
+++ script.js
@@ -0,0 +1,23 @@
+/*
+Attraverso l'apposita API di Boolean
+https://flynn.boolean.careers/exercises/api/random/mail
+generare 10 indirizzi email e stamparli in pagina all'interno di una lista.
+Bonus
+Far comparire gli indirizzi email solamente quando sono stati tutti generati. */
+
+const app= new Vue({
+    el: '#app',
+    data:{
+        allMail:[],
+
+    },
+    mounted(){
+        for (let index = 0; index < 10; index++) {
+            axios.get('https://flynn.boolean.careers/exercises/api/random/mail').then((response)=>{
+                const mail = response.data;
+
+                this.allMail.push(mail.response)
+            });   
+        }
+    }
+})
